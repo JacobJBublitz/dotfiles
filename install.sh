a427a5dc307bf8754953766c939285fff8f8a288
Jacob Bublitz <Jacob@Bublitz.com>
2017-02-03 22:14:15 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #/usr/bin/env sh
 
+# Remove old versions of Oh My Zsh if present
+rm -rf ~./oh-my-zsh
+
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 rm ~/.bash_logout
