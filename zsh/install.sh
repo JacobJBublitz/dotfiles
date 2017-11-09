b41d0bd540d8ca37e4f5ea0462bbf2d06253f853
Jacob Bublitz <jacob@bublitz.com>
2017-11-08 22:19:40 -0800

    Add Oh My Zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,9 @@
 echo "Installing zsh dotfiles..."
+
+# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 cp zshrc.zsh $HOME/.zshrc
 cp zprofile.zsh $HOME/.zprofile
 cp zlogout.zsh $HOME/.zlogout
