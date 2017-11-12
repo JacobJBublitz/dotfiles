fe7926c19d404d0d48065a419031672b5ffdef67
Jacob Bublitz <jacob@bublitz.com>
2017-11-11 22:38:31 -0800

    Add Xresources and a theme

--- x/install.sh
+++ x/install.sh
@@ -5,3 +5,8 @@ echo "Installing Xorg dotfiles..."
 cp xinitrc.sh "$HOME/.xinitrc"
 cp xprofile.sh "$HOME/.xprofile"
 
+cp Xresources "$HOME/.Xresources"
+
+mkdir -p "$HOME/.Xresources.d/themes"
+cp themes/orange "$HOME/.Xresources.d/themes/orange"
+
--- x/update.sh
+++ x/update.sh
@@ -3,3 +3,7 @@
 [[ -f "$HOME/.xinitrc" ]] && cp "$HOME/.xinitrc" xinitrc.sh
 [[ -f "$HOME/.xprofile" ]] && cp "$HOME/.xprofile" xprofile.sh
 
+[[ -f "$HOME/.Xresources" ]] && cp "$HOME/.Xresources" Xresources
+
+[[ -f "$HOME/.Xresources.d/themes/orange" ]] && cp "$HOME/.Xresources.d/themes/orange" themes/orange
+
