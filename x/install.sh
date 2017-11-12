4e0f5234096dec15abde6dba67e420a80cbd1866
Jacob Bublitz <jacob@bublitz.com>
2017-11-11 23:55:52 -0800

    Add URxvt

--- x/install.sh
+++ x/install.sh
@@ -6,6 +6,7 @@ cp xinitrc.sh "$HOME/.xinitrc"
 cp xprofile.sh "$HOME/.xprofile"
 
 cp Xresources "$HOME/.Xresources"
+xrdb merge "$HOME/.Xresources"
 
 mkdir -p "$HOME/.Xresources.d/themes"
 cp themes/orange "$HOME/.Xresources.d/themes/orange"
